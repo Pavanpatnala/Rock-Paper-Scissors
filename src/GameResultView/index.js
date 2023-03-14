f08e3d2e8d99093bb161610b4b51aa6ac5d3917b
pavan <pavankumarpatnala96@gmail.com>
2023-03-14 09:35:03 +0000

    commit message

---
+++ src/GameResultView/index.js
@@ -0,0 +1,42 @@
+import {
+  ShowResultContainer,
+  ResultImagesContainer,
+  ResultTextImgContainer,
+  ResultText,
+  ResultImageItem,
+  ResultButtonContainer,
+  PlayAgainButton,
+} from './styledComponents'
+
+const GameResultView = props => {
+  const {myChoice, apponentChoice, resultMessage, playAgain} = props
+  // eslint-disable-next-line no-unused-vars
+  const {id, image} = apponentChoice
+
+  const onClickPlayAgainButton = () => {
+    playAgain()
+  }
+
+  return (
+    <ShowResultContainer>
+      <ResultImagesContainer>
+        <ResultTextImgContainer>
+          <ResultText>YOU</ResultText>
+          <ResultImageItem src={myChoice[1]} alt="your choice" />
+        </ResultTextImgContainer>
+        <ResultTextImgContainer>
+          <ResultText>OPPONENT</ResultText>
+          <ResultImageItem src={image} alt="opponent choice" />
+        </ResultTextImgContainer>
+      </ResultImagesContainer>
+      <ResultText>{resultMessage}</ResultText>
+      <ResultButtonContainer>
+        <PlayAgainButton type="button" onClick={onClickPlayAgainButton}>
+          Play Again
+        </PlayAgainButton>
+      </ResultButtonContainer>
+    </ShowResultContainer>
+  )
+}
+
+export default GameResultView
